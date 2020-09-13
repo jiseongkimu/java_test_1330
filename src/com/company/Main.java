6c808e65ffbdff948f638131741db19a23250d80
jiseongkimu <jiseongkimu@gmail.com>
2020-09-13 12:23:13 +0900

    for pull

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void Compareto(int a, int b){
+    public static String Compareto(int a, int b){
         if(a>b)
-            System.out.println(">");
+            return ">";
         if(a==b)
-            System.out.println("==");
+            return "==";
         if(a<b)
-            System.out.println("<");
+            return "<";
+
+        return "없음";
     }
 
     public static void main(String[] args) {
@@ -23,16 +25,12 @@ public class Main {
             Scanner sc = new Scanner(System.in);
             System.out.println("비교 할 두 수를 입력하세요(종료 : 0 0 )");
             System.out.println("(제한 : -10,000 ≤ A, B ≤ 10,000)");
-            /* 두 수 입력 */
             intput1 = sc.nextInt();
             intput2 = sc.nextInt();
-
-            /* 종료 */
             if(intput1==0 && intput2==0) {
                 System.out.println("종료합니다");
                 break;
             }
-            /* 범위 밖 수를 입력했을 때 */
             if (-limit > intput1) {
                 System.out.println("첫 번째 수는 " + -limit + "보다 커야합니다.");
                 break;
@@ -41,10 +39,7 @@ public class Main {
                 System.out.println("두 번째 수는 " + limit + "보다 작아야합니다.");
                 break;
             }
-            /* 범위 내 수를 입력했을 때 */
-
-            Compareto(intput1, intput2);
-
+            System.out.println(Compareto(intput1, intput2));
         }
     }
 }
