ef76b351c9597e26c1694407e021ab6844e3e69d
jiseongkimu <jiseongkimu@gmail.com>
2020-09-13 14:55:25 +0900

    함수와 변수의 이름을 다시 정했습니다. 결과 출력시 개행을 하지 않도록 수정했습니다.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,25 @@
 package com.company;
 
-import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
 
-    public static int count(int c, String[] a){
-        for(int i = 0; i < a.length ; i++){
-            if(a[i].equals(" ")){
-                c=c+1;
-            }
+    public static int count_word(int blank, String[] noblank_word){
+        for(int i = 0; i < noblank_word.length ; i++){
+            if(noblank_word[i].equals(" "))
+                blank = blank+1;
         }
-        return c+1;
+        return blank+1;
     }
 
     public static void main(String[] args) {
-        int count = 0;
+        int blank = 0;
         String word;
-        String[] noblank;
+        String[] noblank_word;
         Scanner sc = new Scanner(System.in);
         word = sc.nextLine();
         word = word.trim();
-        noblank = word.split("");
-        System.out.println(count(count, noblank));
+        noblank_word = word.split("");
+        System.out.print(count_word(blank, noblank_word));
     }
 }
