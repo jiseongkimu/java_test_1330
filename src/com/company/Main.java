f4d7351f5e327046bbe21ca491708d2625c3e3d8
jiseongkimu <jiseongkimu@gmail.com>
2020-09-13 12:15:42 +0900

    앗 김지성 리턴값도 출력못한다?!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,14 @@ import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
+    public static int sum(int a, int b){
+        return a+b;
+    }
     public static void main(String[] args) {
         int num1, num2;
         Scanner sc = new Scanner(System.in);
         num1 = sc.nextInt();
         num2 = sc.nextInt();
-        sum(num1, num2);
-    }
-    public static int sum(int a, int b){
-        return a+b;
+        System.out.println(sum(num1, num2));
     }
 }
