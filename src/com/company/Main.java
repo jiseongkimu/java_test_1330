c094eb195c62bafa01d68675bbcb434741493ec7
jiseongkimu <jiseongkimu@gmail.com>
2020-09-14 13:39:26 +0900

    [refactor] 불필요한 변수 제거, 구조 개선

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,33 +11,32 @@ public class Main {
         return array;
     }
 
-    private static int[] bubbleSortArray(int length, int[] array, int temp){
+    private static void bubbleSortArray(int length, int[] array){
         for(int j = 0 ; j < length ; j++){
             for (int i = 0; i < length - 1; i++) {
                 if (array[i] > array[i + 1]) {
-                    temp = array[i + 1];
+                    int temp = array[i + 1];
                     array[i + 1] = array[i];
                     array[i] = temp;
                     //length--;
                 }
             }
         }
-        return array;
     }
 
     private static void printArray(int[] array){
-        for(int i = 0 ; i < array.length ; i++){
-            System.out.print(array[i]+" ");
+        for (int j : array) {
+            System.out.print(j + " ");
         }
     }
 
     public static void main(String[] args) {
         int[] array = new int[5];
-        int temp = 0, length = array.length;
+        int length = array.length;
         Scanner sc = new Scanner(System.in);
 
         array = creatArray(length, array, sc);
-        bubbleSortArray(length, array, temp);
+        bubbleSortArray(length, array);
         printArray(array);
     }
 }
