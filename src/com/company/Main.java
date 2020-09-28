6baecffcdab15b67ea05e8e2e19d6e5105d6de80
jiseongkimu <jiseongkimu@gmail.com>
2020-09-28 12:52:43 +0900

    [feat] Reflection 예제

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,8 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
 
-    public static int count_word(int blank, String[] noblank_word){
-        for(int i = 0; i < noblank_word.length ; i++){
-            if(noblank_word[i].equals(" "))
-                blank = blank+1;
-        }
-        return blank+1;
-    }
-
     public static void main(String[] args) {
-        int blank = 0;
-        String word;
-        String[] noblank_word;
-        Scanner sc = new Scanner(System.in);
-        word = sc.nextLine();
-        word = word.trim();
-        noblank_word = word.split("");
-        System.out.print(count_word(blank, noblank_word));
+
     }
 }
