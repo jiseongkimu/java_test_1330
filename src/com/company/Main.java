1fd075fffa097ba0270ce734a2bcbee9525629f8
jiseongkimu <jiseongkimu@gmail.com>
2020-09-29 22:51:21 +0900

    [feat] String ==, equals 비교

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,27 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        
+        String str1 = "abc";
+        String str2 = "abc";
+        String str3 = new String("abc");
+
+        System.out.print("str1==str2 : ");
+        System.out.println(str1==str2);
+        System.out.println(""+str1==str2); // 앞에 "" 만 넣었는데 왜 결과가 flase가 될까
+
+        System.out.print("str1.equals(str2) : ");
+        System.out.println(str1.equals(str2));
+
+        System.out.print("str1==str3 : ");
+        System.out.println(str1==str3);
+
+        System.out.print("str1.equals.(str3) : ");
+        System.out.println(str1.equals(str3));
+
+        str3 = str3.intern();
+        System.out.print("after str3.inter() str1==str3 : ");
+        System.out.println(str1==str3);
     }
 }
